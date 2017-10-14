59bcfb215f50ba66f76edf9b5609498dc979b811
Clark Fan <fyryhx@gmail.com>
2017-10-14 14:35:28 -0700

    Add step definition

---
+++ features/step_definitions/users_step.rb
@@ -0,0 +1,35 @@
+Given /^(?:|I )am on (.*)$/ do |page_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When /^(?:|I )follow "([^"]*)"$/ do |link|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+Then /^(?:|I )should be on (.+)$/ do |page_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+When /^(?:|I )fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
+  # fill_in(field, :with => value)
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When /^(?:|I )upload "([^"]*)" with "([^"]*)"$/ do |field, path|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then /^(?:|I )should see "([^"]*)"$/ do |text|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+When /I (un)?check the following help: (.*)/ do |uncheck, help_list|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When /^(?:|I )press "([^"]*)"$/ do |button|
+  pending # Write code here that turns the phrase above into concrete actions
+end
