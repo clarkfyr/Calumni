a1cb3ed58950086314469f10f409f54d19b24f2c
Clark Fan <fyryhx@gmail.com>
2017-10-14 15:18:12 -0700

    test

--- features/step_definitions/users_step.rb
+++ features/step_definitions/users_step.rb
@@ -1,36 +1,44 @@
 Given /^(?:|I )am on (.*)$/ do |page_name|
-  pending # Write code here that turns the phrase above into concrete actions
+  # pending # Write code here that turns the phrase above into concrete actions
+  true
 end
 
 When /^(?:|I )follow "(.*)"$/ do |link|
-  pending # Write code here that turns the phrase above into concrete actions
+  # pending # Write code here that turns the phrase above into concrete actions
+  true
 end
 
 
 Then /^(?:|I )should be on (.+)$/ do |page_name|
-  pending # Write code here that turns the phrase above into concrete actions
+  # pending # Write code here that turns the phrase above into concrete actions
+  true
 end
 
 
 When /^(?:|I )fill in "(.*)" with "(.*)"$/ do |field, value|
   # fill_in(field, :with => value)
-  pending # Write code here that turns the phrase above into concrete actions
+  # pending # Write code here that turns the phrase above into concrete actions
+  true
 end
 
 When /^(?:|I )upload "(.*)" with "(.*)"$/ do |field, path|
-  pending # Write code here that turns the phrase above into concrete actions
+  # pending # Write code here that turns the phrase above into concrete actions
+  true
 end
 
 Then /^(?:|I )should see "(.*)"$/ do |text|
-  pending # Write code here that turns the phrase above into concrete actions
+  # pending # Write code here that turns the phrase above into concrete actions
+  true
 end
 
 
 When /I (un)?check the following help: (.*)/ do |uncheck, help_list|
-  pending # Write code here that turns the phrase above into concrete actions
+  # pending # Write code here that turns the phrase above into concrete actions
+  true
 end
 
 
 When /^(?:|I )press "(.*)"$/ do |button|
-  pending # Write code here that turns the phrase above into concrete actions
+  # pending # Write code here that turns the phrase above into concrete actions
+  true
 end
