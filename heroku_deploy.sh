8ceecbad51d8ccadfff2869d2538a2f89de29557
makeapp <makeapp007@gmail.com>
2017-10-22 23:14:49 +0000

    add deploy

---
+++ heroku_deploy.sh
@@ -0,0 +1,4 @@
+heroku config:set GOOGLE_CLIENT_ID=74132573151-rpps9aksj9sbci071dgbmec6sj969ajh.apps.googleusercontent.com
+heroku config:set GOOGLE_CLIENT_SECRET=o2J-bgMzZBDM-_QtTUlzIHuY
+heroku restart
+
