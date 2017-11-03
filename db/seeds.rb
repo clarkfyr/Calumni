85c5284d0865d73f8c3c8b7442c0b3c8f7ced415
Clark Fan <fyryhx@gmail.com>
2017-11-02 23:14:45 -0700

    Iter msg (#6)
    * test
    
    * deploy
    
    * fix style
    
    * fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ root=Rails.root.to_s
 # m1={:username=>"Ma yi",:email=>"yima@uiuc.edu",:avatar=>File.new("#{root}/app/assets/images/mayi.jpeg")}
 
 people = [{:username=>"Ma yi",:email=>"yima@uiuc.edu",:description =>"dean of SIST ", :company=>"ShanghaiTech",:start_date => '2014/1/2', :avatar=>File.new("#{root}/app/assets/images/mayi.jpeg"),:position=>'professor'},
-	{:username => 'steve jobs', :email => '1@gmail.com', :description => 'this is description1',:company => 'Apple',:start_date => '2017/1/2',:major=>'EECS',:position=>'CEO',:open_advice=>'advice 1',:avatar=>File.new("#{root}/app/assets/images/jobs.jpg")},
+	{:username => 'steve jobs', :email => '1@gmail.com', :description => 'this is description1',:company => 'Apple',:start_date => '2017/1/2',:major=>'EECS',:position=>'CEO',:open_advice=>'advice 1',:helpability=>'resume',:avatar=>File.new("#{root}/app/assets/images/jobs.jpg")},
 	{:username => 'taylor swift', :email => '2@gmail.com', :description => 'this is description2',:company => 'Amazon',:start_date => '2017/11/1',:major=>'CS',:open_advice=>'advice 2',:avatar=>File.new("#{root}/app/assets/images/taylor.jpg"),:position=>'singer'},
 {:username => 'sam', :email => '3@gmail.com', :description => 'this is description3',:company => 'YC',:start_date => '2017/9/2',:major=>'Biology',:open_advice=>'advice 3',:avatar=>File.new("#{root}/app/assets/images/sam.jpg"),:position=>'investor'},
 {:username => 'unknown', :email => '4@gmail.com', :description => 'this is description4',:company => 'Ali',:start_date => '2015/1/2',:major=>'EECS',:open_advice=>'advice 4',:avatar=>File.new("#{root}/app/assets/images/hero.jpg")},:position=>'COO']
