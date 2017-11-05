6bf963c71fc1349a2f0ea0c7f93329f9a01e89f7
makeapp <makeapp007@gmail.com>
2017-11-05 03:49:28 +0000

    add hostname for aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage=> :s3,
     :bucket => ENV['S3_BUCKET_NAME'],
+    :s3_host_name => 's3.us-east-2.amazonaws.com',
     :s3_credentials=> {
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
