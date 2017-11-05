a9e8ee96a76f89267c2a59ae2bcac764d3ff1ad1
makeapp <makeapp007@gmail.com>
2017-11-05 04:12:16 +0000

    force resources on https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,7 @@ Rails.application.configure do
     :storage=> :s3,
     :bucket => ENV['S3_BUCKET_NAME'],
     :s3_host_name => 's3.us-east-2.amazonaws.com',
+    :s3_protocol => :https,
     :s3_credentials=> {
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
