e5594669d0b8c79243a68d483ea4ebb35583408b
makeapp <liuyh1@shanghaitech.edu.cn>
2017-11-09 12:57:05 -0800

    Almost there

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,7 +1,63 @@
 $(function() {
-  return $('#username_search').typeahead({
-    name: "book",
-    remote: "autocomplete?query=%QUERY"
-  });
+// var bestPictures = new Bloodhound({
+//   datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+//   queryTokenizer: Bloodhound.tokenizers.whitespace,
+//   remote: {
+//   	url: "autocomplete?query=%QUERY",
+//   	wildcard: "%QUERY"
+//   }
+// });
+
+// bestPictures.initialize();
+
+// $('#username_search').typeahead(null, {
+//   name: 'best-pictures',
+//   displayKey: 'username',
+//   source: bestPictures.ttAdapter()
+// });
+// $('#username_search').bind('typeahead:selected', function(event, datum, name) {
+// 	console.log("sdf")
+  
+//   // doSomething(datum.id);
+// });
+
+
+// $('#username_search').typeahead({                              
+//   name: 'example',                                                             
+//   local: [
+//         'Salt Lake City',
+//         'Provo',
+//         'Ogden',
+//         'Bountiful',
+//         'Orem',
+//         'Centerville',
+//         'St. George',
+//         'Cedar City',
+//         'Hurricane',
+//   ]                                                                           
+// }); 
+
+var numbers = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.whitespace,
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  local:  ["(A)labama","Alaska","Arizona","Arkansas","Arkansas2","Barkansas"]
 });
+
+// initialize the bloodhound suggestion engine
+numbers.initialize();
+
+$("#username_search").typeahead({
+  items: 4,
+  source:numbers.ttAdapter()
+});
+
+  // $('#username_search').typeahead({
+	
+  //   name: "book",
+  //   remote: "autocomplete?query=%QUERY"
+  // });
+
+
+});
+
 
