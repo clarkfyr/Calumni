56055f9949517b0ee8898ceba62061b3f185339a
makeapp <makeapp007@gmail.com>
2017-11-10 07:36:42 +0000

    a

--- app/models/people.rb
+++ app/models/people.rb
@@ -2,6 +2,7 @@ require 'elasticsearch/model'
 class People < ActiveRecord::Base
    include Elasticsearch::Model
    include Elasticsearch::Model::Callbacks
+    Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL'], log: true
    searchkick autocomplete: ['username',"company"]
    has_attached_file :avatar,styles: { medium: "300x300>", thumb: "100x100>" }
    do_not_validate_attachment_file_type :avatar
