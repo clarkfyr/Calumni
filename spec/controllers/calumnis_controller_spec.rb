382a239ce5240506ca1d8e59f26cae4d50951931
Hanxiao Deng <hxdeng@berkeley.edu>
2017-11-19 22:01:08 -0800

    debug

--- spec/controllers/calumnis_controller_spec.rb
+++ spec/controllers/calumnis_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe CalumnisController, type: :controller do
+  describe "GET index" do
+    it "assigns @teams" do
+      people = People.create
+      get :upload
+      expect(assigns(:teams)).to eq([team])
+    end
 
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
 end
