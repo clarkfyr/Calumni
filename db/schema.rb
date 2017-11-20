0ae8e68c5704b18df02e134f0f71cb75be6c6e78
Yingying Chen <ychen1030@berkeley.edu>
2017-11-19 16:12:27 -0800

    refactor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108055144) do
+ActiveRecord::Schema.define(version: 20171119052700) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20171108055144) do
     t.string   "help_status"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "help_type"
   end
 
   create_table "messages", force: :cascade do |t|
