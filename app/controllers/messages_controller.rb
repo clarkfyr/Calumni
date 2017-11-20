88032c6f6d2390424c0766a60e44d543f9a9f30c
xuranzhang <xuranzhang75@gmail.com>
2017-11-20 00:01:28 -0800

    [Style Fix] messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -36,29 +36,27 @@ end
 def create
     @message = @conversation.messages.new(message_params)
     @selected_help = params[:helps] || {}
-    @help_responds = params[:responds] || {}
-    @people= People.select{|p| p.email==cookies[:email]}
+    # @help_responds = params[:responds] || {}
+    # @people= People.select{|p| p.email==cookies[:email]}
 
-    if @help_responds != {} and @help_responds.keys[0] == "Done"
-        @conversation.update_attribute(:help_status, @help_responds.keys[0])
-
-        @people.first.update_attribute(:helped_count, @people.first.helped_count+1)
-    elsif @help_responds != {}
-        @conversation.update_attribute(:help_status, @help_responds.keys[0])
-
+    # if @help_responds != {} and @help_responds.keys[0] == "Done"
+    #     @conversation.update_attribute(:help_status, @help_responds.keys[0])
+    #
+    #     @people.first.update_attribute(:helped_count, @people.first.helped_count+1)
+    # elsif @help_responds != {}
+    #     @conversation.update_attribute(:help_status, @help_responds.keys[0])
+    if respond_to_help != false
+        p 1
     elsif message_params[:body].blank?
         flash[:notice] = "Please do not send empty message."
-
     elsif @selected_help&.keys.size > 1
         flash[:notice] = "Please do not choose more than one help type."
 
-
-
     elsif @selected_help != {}
-
-        @conversation.update_attribute(:help_type, @selected_help.keys[0])
-        @conversation.update_attribute(:help_status, "Pending")
-        @conversation.update_attribute(:requester, current_user.id)
+        update_help
+        # @conversation.update_attribute(:help_type, @selected_help.keys[0])
+        # @conversation.update_attribute(:help_status, "Pending")
+        # @conversation.update_attribute(:requester, current_user.id)
     elsif !@message.save
         return
     end
@@ -75,5 +73,27 @@ def message_params
     params.require(:message).permit(:body, :people_id)
 end
 
+
+
+def update_help
+    @selected_help = params[:helps] || {}
+    @conversation.update_attribute(:help_type, @selected_help.keys[0])
+    @conversation.update_attribute(:help_status, "Pending")
+    @conversation.update_attribute(:requester, current_user.id)
+end
+
+def respond_to_help
+    @people= People.select{|p| p.email==cookies[:email]}
+    @help_responds = params[:responds] || {}
+    if @help_responds != {} and @help_responds.keys[0] == "Done"
+        @conversation.update_attribute(:help_status, @help_responds.keys[0])
+        @people.first.update_attribute(:helped_count, @people.first.helped_count+1)
+        return true
+    elsif @help_responds != {}
+        @conversation.update_attribute(:help_status, @help_responds.keys[0])
+        return true
+    end
+    return false
+end
 
 end
