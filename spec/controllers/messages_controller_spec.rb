cf278c375d6f94c028f277a31c8291d59a51dae5
Hanxiao Deng <hxdeng@berkeley.edu>
2017-12-02 17:16:17 -0800

    test

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe MessagesController, type: :controller do
+  describe "create" do
+    it "can create" do
+#      controller.create
+    end
+  end
+end
