e1bd144293bf1ce31a0b0796b2852dce6381b899
xuranzhang <xuranzhang75@gmail.com>
2017-12-03 19:18:23 -0800

    [fix] fix code smell

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -40,16 +40,16 @@ def create
         p 1
     elsif @selected_help != {}
         update_help
-    #elsif @message.save
-    #    sync_new @message, scope: @conversation
-    elsif !@message.save
-         return
+    elsif @message.save
+       sync_new @message, scope: @conversation
+    # elsif !@message.save
+    #      return
     end
 
 
-    if @message.save
-      sync_new @message, scope: @conversation
-    end
+    # if @message.save
+    #   sync_new @message, scope: @conversation
+    # end
 
     p @conversation 
 
